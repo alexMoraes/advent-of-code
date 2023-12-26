f136b9214e17228698d2c33986d7d23d3cecdcc6
Alex Moraes <alexmoraes251@gmail.com>
2023-12-26 17:33:48 -0300

    Solutions for day 13 2023 (part 2)

--- 2023/app/day13/Main.hs
+++ 2023/app/day13/Main.hs
@@ -5,27 +5,28 @@ import qualified Data.Text as T
 
 import Paths_adventofcode2023 (getDataFileName)
 
-isReflection :: Int -> String -> Bool
-isReflection n l = all (uncurry (==)) . zip (reverse . take n $ l) $ drop n l
+countDiffs :: Int -> String -> Int
+countDiffs n l = length . filter (uncurry (/=)) . zip (reverse . take n $ l) $ drop n l
 
-findReflection :: [String] -> Maybe Int
-findReflection ls = do
+findReflection :: Int -> [String] -> Maybe Int
+findReflection smudges ls = do
   let getMaxLen [] = 0
       getMaxLen (h:_) = length h
   let maxLen = getMaxLen ls
-  find (\c -> all (isReflection c) ls) [1..maxLen-1]
+  find (\c -> (smudges ==) . sum . map (countDiffs c) $ ls) [1..maxLen-1]
 
-summarizePattern :: [String] -> Int
-summarizePattern ls = case findReflection ls of
+summarizePattern :: Int -> [String] -> Int
+summarizePattern smudges ls = case findReflection smudges ls of
   Just s -> s
-  Nothing -> (100 *) . fromMaybe 0 . findReflection . transpose $ ls
+  Nothing -> (100 *) . fromMaybe 0 . findReflection smudges . transpose $ ls
 
-summarize :: [[String]] -> Int
-summarize = sum . map summarizePattern
+summarize :: Int -> [[String]] -> Int
+summarize smudges = sum . map (summarizePattern smudges)
 
 main :: IO ()
 main = do
   path <- getDataFileName "day13\\input.txt"
   contents <- readFile path
   let patterns = (map (lines . T.unpack) . T.splitOn "\n\n" . T.pack) contents
-  print ("First solution: " ++ (show . summarize $ patterns))
+  print ("First solution: " ++ (show . summarize 0 $ patterns))
+  print ("Second solution: " ++ (show . summarize 1 $ patterns))
